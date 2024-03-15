93683cdf1a78465bc7a4f092da1c65208db259c1
Jithin Vinod <72181381+jithinvinod72@users.noreply.github.com>
2024-03-15 18:49:00 -0400

    cocah-quitainaire api

--- src/app.js
+++ src/app.js
@@ -3,21 +3,42 @@ const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const path = require('path');
+const { createClient } = require('@supabase/supabase-js');
+
 const swaggerDocument = YAML.load(path.join(__dirname, './doc/swagger.yaml'));
 const questionsData = require('./json/questions');
+const coachQuestionsData = require('./json/coachQuestions');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+require('dotenv').config();
+
+// Configure Supabase
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+//questions
 app.get('/questions', (req, res) => {
     try {
         res.status(200).json(questionsData);
     } catch (error) {
         console.error("Failed to retrieve questions data:", error);
         res.status(500).send({ message: "Failed to retrieve questions data." });
+    }
+});
+
+
+app.get('/coach-questions', (req, res) => {
+    try {
+        res.status(200).json(coachQuestionsData);
+    } catch (error) {
+        console.error("Failed to retrieve coach questions data:", error);
+        res.status(500).send({ message: "Failed to retrieve coach questions data." });
     }
 });
 
