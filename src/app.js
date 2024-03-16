20a31436d373e993f7792461a82088494b72fd80
Jithin Vinod <72181381+jithinvinod72@users.noreply.github.com>
2024-03-16 08:51:18 -0400

    User Save api completed

--- src/app.js
+++ src/app.js
@@ -42,6 +42,66 @@ app.get('/coach-questions', (req, res) => {
     }
 });
 
+//user
+//#region  USER
+app.post('/save-user', async (req, res) => {
+    try {
+        const { firstName, lastName, email, password, userRoleId, userData } = req.body;
+
+        // Initial input validation for null values
+        if (!firstName || !lastName || !email || !password || !userRoleId || !userData) {
+            return res.status(400).json({ message: "Missing required fields." });
+        }
+
+        // Insert into Users table
+        const { data: userArray, error: userError } = await supabase
+            .from('Users')
+            .insert([
+                {
+                    "FirstName": firstName,
+                    "LastName": lastName,
+                    "Email": email,
+                    password,
+                    "userRoleId": userRoleId
+                }
+            ]).select('*');
+
+        if (userError) throw new Error(userError.message);
+        if (!userArray || userArray.length === 0) throw new Error('User insertion failed.');
+
+        const userId = userArray[0].id;
+
+        const userDataModel = {
+            "userId": userId,
+            ...userData
+        };
+
+        // Insert into UserData table
+        const { data: userInfoArray, error: userDataError } = await supabase
+            .from('UserData')
+            .insert([userDataModel]).select('*');
+
+        if (userDataError) throw new Error(userDataError.message);
+        if (!userInfoArray || userInfoArray.length === 0) throw new Error('UserData insertion failed.');
+
+        // Constructing the response data with null checks
+        const responseData = {
+            user: userArray ? userArray[0] : {},
+            userInfo: userInfoArray ? userInfoArray[0] : {}
+        };
+
+        res.status(200).json({
+            message: "User and related data saved successfully.",
+            data: responseData
+        });
+    } catch (error) {
+        console.error("Failed to save user data:", error);
+        res.status(500).json({ message: "Failed to save user data.", error: error.message });
+    }
+});
+
+
+//#endregion
 
 const PORT = process.env.PORT || 3000;
 
